5a9025ffc43f1c85d58fef5c5c919c930992a6c8
02lyx <yuanxinliu2029@u.northwestern.edu>
2024-12-28 13:45:44 -0600

    update to qp_version

--- gemma-2-9b-it.sh
+++ gemma-2-9b-it.sh
@@ -31,14 +31,14 @@ run_iteration() {
     infer_model="${e_model_dir}/final_checkpoint"
     prompt_dir=$3
     output_dir=$4
-    CUDA_VISIBLE_DEVICES=0 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 0 --my_world_size ${my_world_size} &
-    CUDA_VISIBLE_DEVICES=1 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 1 --my_world_size ${my_world_size} &
-    CUDA_VISIBLE_DEVICES=2 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 2 --my_world_size ${my_world_size} &
-    CUDA_VISIBLE_DEVICES=3 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 3 --my_world_size ${my_world_size} &
-    CUDA_VISIBLE_DEVICES=4 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 4 --my_world_size ${my_world_size} &
-    CUDA_VISIBLE_DEVICES=5 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 5 --my_world_size ${my_world_size} &
-    CUDA_VISIBLE_DEVICES=6 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 6 --my_world_size ${my_world_size} &
-    CUDA_VISIBLE_DEVICES=7 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 7 --my_world_size ${my_world_size} &
+    CUDA_VISIBLE_DEVICES=0 python ./generation/gen_batch.py --model_q_name_or_path ${infer_model} --model_p_name_or_path ${model_path} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 0 --my_world_size ${my_world_size} &
+    CUDA_VISIBLE_DEVICES=1 python ./generation/gen_batch.py --model_q_name_or_path ${infer_model} --model_p_name_or_path ${model_path} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 1 --my_world_size ${my_world_size} &
+    CUDA_VISIBLE_DEVICES=2 python ./generation/gen_batch.py --model_q_name_or_path ${infer_model} --model_p_name_or_path ${model_path} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 2 --my_world_size ${my_world_size} &
+    CUDA_VISIBLE_DEVICES=3 python ./generation/gen_batch.py --model_q_name_or_path ${infer_model} --model_p_name_or_path ${model_path} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 3 --my_world_size ${my_world_size} &
+    CUDA_VISIBLE_DEVICES=4 python ./generation/gen_batch.py --model_q_name_or_path ${infer_model} --model_p_name_or_path ${model_path} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 4 --my_world_size ${my_world_size} &
+    CUDA_VISIBLE_DEVICES=5 python ./generation/gen_batch.py --model_q_name_or_path ${infer_model} --model_p_name_or_path ${model_path} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 5 --my_world_size ${my_world_size} &
+    CUDA_VISIBLE_DEVICES=6 python ./generation/gen_batch.py --model_q_name_or_path ${infer_model} --model_p_name_or_path ${model_path} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 6 --my_world_size ${my_world_size} &
+    CUDA_VISIBLE_DEVICES=7 python ./generation/gen_batch.py --model_q_name_or_path ${infer_model} --model_p_name_or_path ${model_path} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 30 --temperature 1.0 --local_index 7 --my_world_size ${my_world_size} &
     wait
     python ./generation/merge_data.py --base_path ${output_dir} --output_dir "${output_dir}_data.json" --num_datasets $my_world_size
 
